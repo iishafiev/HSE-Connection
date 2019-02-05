cfdaec0c750ee09896c0a4cb745f9b7ebdaa0120
RudyWhale <poetry.free@yandex.ru>
2019-02-05 20:43:05 +0300

    Made some initial work

--- hse_connection/main/models.py
+++ hse_connection/main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+'''
+Stores user information
+'''
+class UserProfile(models.Model):
+	CAMPUS_CHOICES = [
+		('Msc','Moscow'),
+		('Spb','Saint-Petersburg'),
+		('NNvgd','Nizhny Novgorod'),
+		('Perm','Perm')
+	]
+
+	email = models.EmailField()
+	position = models.CharField(max_length=250)
+	avatar = models.ImageField()
+	campus = models.CharField(choices=CAMPUS_CHOICES, max_length=100)
+
+
+'''
+Represents the publication
+'''
+class Publication(models.Model):
+	author = models.ForeignKey(User, on_delete = models.CASCADE)
+	name = models.CharField(max_length=250)
+	text = models.TextField()
+	date = models.DateTimeField()
