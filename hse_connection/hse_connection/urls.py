43eea8a690b05da136558a954bc7fb66ef7151f9
RudyWhale <poetry.free@yandex.ru>
2019-02-15 10:28:38 +0300

    Made working login page

--- hse_connection/hse_connection/urls.py
+++ hse_connection/hse_connection/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
 	path('', include('main.urls')),
+	path('auth/', include('login.urls')),
     path('admin/', admin.site.urls),
 ]
--- hse_connection/login/views.py
+++ hse_connection/login/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login(request):
+	# Checks received user data
+	if request.method == 'POST':
+	    username = request.POST['inputEmail']
+	    password = request.POST['inputPassword']
+	    user = authenticate(request, username=username, password=password)
+	    if user:
+	        login(request, user)
+	        return redirect('index')
+	    else:
+	        return redirect('index')
+
+    # Creates the login form
+	else:
+		return render(request, 'login/signin.html')
